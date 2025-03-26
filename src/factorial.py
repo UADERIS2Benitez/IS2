a2c1b55e219d35b7d51abd8664c109f75c73ee3c
UADERIS2Benitez <jesuniko25@gmail.com>
2025-03-26 13:10:27 -0300

    Factorial2.0

--- src/factorial.py
+++ src/factorial.py
@@ -1,28 +1,38 @@
 #!/usr/bin/python
-#*-------------------------------------------------------------------------*
-#* factorial.py                                                            *
-#* calcula el factorial de un número                                       *
-#* Dr.P.E.Colla (c) 2022                                                   *
-#* Creative commons                                                        *
-#*-------------------------------------------------------------------------*
+# *-------------------------------------------------------------------------*
+# * factorial.py                                                            *
+# * calcula el factorial de un número                                       *
+# * Dr.P.E.Colla (c) 2022                                                   *
+# * Creative commons                                                        *
+# *-------------------------------------------------------------------------*
 import sys
-def factorial(num): 
-    if num < 0: 
+
+def factorial(num):
+    if num < 0:
         print("Factorial de un número negativo no existe")
         return 0
-    elif num == 0: 
+    elif num == 0:
         return 1
-        
-    else: 
+    else:
         fact = 1
-        while(num > 1): 
-            fact *= num 
+        while num > 1:
+            fact *= num
             num -= 1
-        return fact 
+        return fact
 
-if len(sys.argv) == 0:
-   print("Debe informar un número!")
-   sys.exit()
-num=10
-print("Factorial ",num,"! es ", factorial(num)) 
+# Verificar si hay argumentos pasados
+if len(sys.argv) > 1:
+    try:
+        num = int(sys.argv[1])
+    except ValueError:
+        print("Debe ingresar un número válido como argumento.")
+        sys.exit()
+else:
+    try:
+        num = int(input("Ingrese un número para calcular su factorial: "))
+    except ValueError:
+        print("Debe ingresar un número válido.")
+        sys.exit()
 
+# Calcular y mostrar el factorial
+print("Factorial", num, "! es", factorial(num))
